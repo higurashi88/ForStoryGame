bf5565709a9cb6ddd60cb7b24e7bcbf0bccf8de5
higurashi88 <msippc1997@gmail.com>
2019-01-28 23:24:17 +0900

    add background

---
+++ Assets/chgSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chgSprite : MonoBehaviour {
+    public Sprite one;
+    public Sprite two;
+
+	// Use this for initialization
+	void Start () {
+	}	
+	// Update is called once per frame
+	void Update () {	
+	}
+    public void changeSprite()
+    {
+        this.gameObject.GetComponent<Image>().sprite = two;
+    }
+}
